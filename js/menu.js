da19cf937a43975d07a2b3a3d3aa058583757be2
Fernando Kalke <fernandokalke97@gmail.com>
2022-09-21 11:57:39 -0300

    ajustes na responsividade

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btnTop.style.display = "block";
+    btnTop.style.display = "flex";
   } else {
     btnTop.style.display = "none";
   }
